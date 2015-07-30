c4cf52954958622c499c275aa6134341b1f209aa
zhampiino <zhampiino@gmail.com>
2015-07-30 13:33:51 +0700

    css/js

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -1,10 +1,46 @@
+//Global Variable
+var resizeId;
+
+//DOM Ready
 $(document).ready(function () {
+
+	setImageRatio('.wrap-aspect-ratio');
 	
-	$('.ellipsis-wrapper').ellipsis({
-		lines: 2,
-		responsive: true 
-	});
+});
+
+//WINDOW Load
+$(window).load(function() {
 	
 	
 });
 
+//WINDOW Resize
+$(window).resize(function() {
+	
+    clearTimeout(resizeId);
+    resizeId = setTimeout(function() {
+    	
+    	setImageRatio('.wrap-aspect-ratio'); 
+    		
+    }, 300);
+    
+    console.log(resizeId);
+	
+});
+
+var setImageRatio = function(img_selector){
+	
+	$(img_selector).each(function() {  
+		var width = $(this).width();   // Current image width
+		var ratio = $(this).data('ratio'); // aspect ratio
+		var height = width * ratio;  // Reset height to match scaled image
+
+		$(this).css("height", height); 
+		console.log('height = '+height+' Ratio = '+ratio);
+		
+	});
+	
+}
+
+
+ 
